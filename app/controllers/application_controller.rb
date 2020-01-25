8ea02f8008b2b7db814979044878aad7273a0ded
narugit <t.naruhiro.1026@gmail.com>
2020-01-25 17:56:42 +0900

    change before checkout dev branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
+  # a code for the CSRF 
   protect_from_forgery with: :exception
 
+  # set the unlogin user can only access sign-up page
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  # set the strong paramater
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #get 'list/new'
   #get 'list/create'
   root 'top#index'
+  # add the users with devise
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :list, only: %i(new create edit update destroy) do
