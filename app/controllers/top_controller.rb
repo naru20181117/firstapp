1fd754be9bd730c7dc48d46c8cc56c38b5e03eca
narugit <t.naruhiro.1026@gmail.com>
2020-01-25 23:47:45 +0900

    add comments

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
+    # to get the list which is owned by the current_user
     @lists = List.where(user: current_user).order("created_at ASC")
   end
 end
